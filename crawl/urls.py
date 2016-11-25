7c8036b0e89fe95296e5a5e6530b75f2a660dc95
zeeshan <zkhan1093@gmail.com>
2016-11-26 00:38:09 +0530

    refacto: added login and crawl view

--- crawl/urls.py
+++ crawl/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url,include
 from . import views
 urlpatterns=[
-	url(r'^$',views.user_login,name="user_login"),
+	url(r'^$',views.home,name="home"),
+	url(r'^login/$',views.user_login,name="user_login"),
+	url(r'^crawl$',views.crawl,name="crawl"),
+	url(r'^start$',views.start_crawling,name="start_crawling"),
 ]
--- crawl/views.py
+++ crawl/views.py
@@ -1,5 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from .models import *
 
 # Create your views here.
+def home(request):
+	if request.user.is_authenticated():
+		return redirect('/crawl')
+	return render(request,'crawl/user_login.html');
+
 def user_login(request):
-	return render(request,'crawl/user_login.html',{});
+	data=dict()
+	if request.user.is_authenticated():
+		return redirect('/crawl')
+	if request.method!='POST':
+		data['error']=True
+		data['msg']='Invalid request type'
+		return render(request,'crawl/user_login.html',data)	
+	username=request.POST['username']
+	password=request.POST['password']
+	user=authenticate(username=username,password=password)
+	if user:
+		login(request,user)
+		return redirect('/crawl')
+	else:
+		data['error']=True
+		data['msg']='Invalid username'
+	return render(request,'crawl/user_login.html',data)
+
+@login_required
+def crawl(request):
+	data=dict()
+	return render(request,'crawl/crawl.html',data)
+
+@login_required
+def start_crawling(request):
+	return render(request,'okay ')
