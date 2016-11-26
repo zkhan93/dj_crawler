43a0651e7b4d333368e2d8dfa4ee32f2290af527
zeeshan <zkhan1093@gmail.com>
2016-11-26 17:15:57 +0530

    refacot: Created models and relation between them

--- crawl/admin.py
+++ crawl/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+class ContentIdentifierAdmin(admin.ModelAdmin):
+	pass
+class InputAdmin(admin.ModelAdmin):
+	pass
+class PostAdmin(admin.ModelAdmin):
+	pass
+class CategoryAdmin(admin.ModelAdmin):
+	pass
+admin.site.register(models.ContentIdentifier,ContentIdentifierAdmin)
+admin.site.register(models.Input,InputAdmin)
+admin.site.register(models.Post,PostAdmin)
+admin.site.register(models.Category,CategoryAdmin)
--- crawl/models.py
+++ crawl/models.py
@@ -2,4 +2,27 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class ContentIdentifier(models.Model):
+	dom=models.CharField(max_length=30)
+	css_selector=models.CharField(max_length=100)
+
+class Input(models.Model):
+	name=models.CharField(max_length=100)
+	created_on=models.DateTimeField(auto_now=True)
+	updated_on=models.DateTimeField(auto_now=True)
+	url=models.CharField(max_length=300)
+	content_identifiers=models.ManyToManyField(ContentIdentifier)
+
+class Post(models.Model):
+	title=models.CharField(max_length=200)
+	summary=models.TextField()
+	link=models.CharField(max_length=300)
+	input=models.ForeignKey(Input,on_delete=models.CASCADE)
+
+class Category(models.Model):
+	category=models.CharField(max_length=100)
+	title=models.CharField(max_length=200)
+	summary=models.TextField()
+	created_on=models.DateTimeField(auto_now=True)
+	updated_on=models.DateTimeField(auto_now=True)
+	posts=models.ManyToManyField(Post)
